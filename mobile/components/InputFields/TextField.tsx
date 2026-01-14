fb358215605cdb87b000a5745b24ce2577f11c5f
sujan0629 <sujanbhatta0629@gmail.com>
2026-01-15 03:50:26 +0545

    changed the inputbox's design

--- mobile/components/InputFields/TextField.tsx
+++ mobile/components/InputFields/TextField.tsx
@@ -16,7 +16,7 @@ export const TextField = ({ label, placeholder, value, onChangeText, multiline, 
     {label ? <Text className="text-sm text-gray-600">{label}</Text> : null}
     <View className="mt-2 rounded-2xl border border-gray-300 bg-white px-4 py-4 flex-row items-center">
       <TextInput
-        className="flex-1 text-black text-sm"
+        className="flex-1 text-black text-base leading-5"
         placeholder={placeholder}
         placeholderTextColor="#9CA3AF"
         value={value}
